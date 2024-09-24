5d12bea50e76d987203bc5e8846a105d01d2ef7f
friday <piotr.piiatek@gmail.com>
2024-09-24 12:13:14 +0200

    wezterm: open maximized, deafult prog for local domain changed to pwsh

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,13 +2,15 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 local act = wezterm.action
--- local mux = wezterm.mux()
+local mux = wezterm.mux
 
 -- APPEARANCE --
 -- config.color_scheme = 'N0tch2k'
-config.color_scheme = "nord"
+config.color_scheme = "Gruvbox Dark (Gogh)"
 config.colors = {
-	background = "#1c1c1c",
+	background = "#11100d",
+	cursor_bg = "#f29718",
+	cursor_border = "#f29718",
 }
 
 config.window_padding = {
@@ -18,15 +20,15 @@ config.window_padding = {
 	left = 10,
 }
 
-config.enable_scroll_bar = true
+config.enable_scroll_bar = false
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
-config.hide_tab_bar_if_only_one_tab = true
+-- config.hide_tab_bar_if_only_one_tab = true
 config.freetype_load_target = "HorizontalLcd"
 config.audible_bell = "Disabled"
 
 config.font = wezterm.font({
-	family = "CaskaydiaCove Nerd Font Mono",
+	family = "Cascadia Code NF",
 	style = "Normal",
 	harfbuzz_features = { "calt=0", "zero" },
 })
@@ -35,6 +37,12 @@ config.bold_brightens_ansi_colors = "BrightOnly"
 
 wezterm.on("update-right-status", function(window, pane)
 	window:set_right_status(window:active_workspace())
+end)
+
+wezterm.on("gui-startup", function(window)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	local gui_window = window:gui_window()
+	gui_window:maximize()
 end)
 
 config.leader = { key = "\\", mods = "CTRL", timeout_milliseconds = 1000 }
@@ -108,6 +116,7 @@ config.unix_domains = {
 -- }
 
 config.default_domain = "WSL:Ubuntu"
+config.default_prog = { "pwsh.exe", "-NoLogo" }
 
 -- config.default_gui_startup_args = { 'connect', 'unix' }
 
